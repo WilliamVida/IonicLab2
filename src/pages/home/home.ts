f11fca1f6f55dc036655b3e8bff35ead4986f27d
WILLIAM VIDA <williamvidaisgod@gmail.com>
2019-03-11 17:32:12 +0000

    ionic lab 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+movies: any = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private movieProvider: MovieProvider) {
 
   }
+
+  ionViewDidLoad() {
+    this.movieProvider.getFilmData().subscribe((data)=>{
+      this.movies = data.Search;
+    });
+   }
 
 }
